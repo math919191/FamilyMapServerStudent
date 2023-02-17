957ce044bcf78c518a865eb7e58d45bc03915498
Rebekah Daniels <math919191@gmail.com>
2023-02-17 16:14:40 -0700

    website now working

--- src/main/java/Handler/FileHandler.java
+++ src/main/java/Handler/FileHandler.java
@@ -1,13 +1,63 @@
 package Handler;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.nio.file.Files;
 
 public class FileHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        boolean success = false;
+        System.out.printf("made it to handle");
+        try {
+            if (exchange.getRequestMethod().toLowerCase().equals("get")) {
+                String urlPath = exchange.getRequestURI().toString();
+//                if (urlPath == "/" || urlPath == null)
+                urlPath = "/index.html";
+                String filePath = "web" + urlPath;
+                File file = new File(filePath);
+                if (!file.exists()){
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
+                    exchange.getResponseBody().close();
+                }
 
+                OutputStream respBody = exchange.getResponseBody();
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+                Files.copy(file.toPath(), respBody);
+//                writeString(, respBody);
+
+                respBody.close();
+
+                success = true;
+
+
+            }
+
+            if (!success) {
+                exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+                exchange.getResponseBody().close();
+            }
+        }
+        catch (IOException e) {
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_SERVER_ERROR, 0);
+            exchange.getResponseBody().close();
+            e.printStackTrace();
+        }
     }
+
+    private void writeString(String str, OutputStream os) throws IOException {
+        OutputStreamWriter sw = new OutputStreamWriter(os);
+        sw.write(str);
+        sw.flush();
+    }
+
+
+
 }
